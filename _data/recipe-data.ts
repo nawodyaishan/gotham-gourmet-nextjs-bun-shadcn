ce54f34d033b7d2d0426e6fc6f0552baf09fbc9f
Nawodya Ishan <nawodyain@gmail.com>
2024-01-07 15:35:14 +0530

    feat: add data

---
+++ _data/recipe-data.ts
@@ -0,0 +1,61 @@
+interface IRecipeData {
+  id: number;
+  title: string;
+  image: string;
+  time: string;
+  description: string;
+  isVegan: boolean;
+}
+
+export const recipeData: IRecipeData[] = [
+  {
+    id: 1,
+    title: 'Batwing Tacos',
+    image:
+      'Description: Crispy, bat-shaped taco shells filled with spiced black beans, purple cabbage, and a tangy lime dressing.',
+    time: '30 minutes',
+    description:
+      'Enjoy a gothic twist on classic tacos with these Batwing Tacos. The shells are creatively shaped like bat wings, adding a playful touch to your meal.',
+    isVegan: true,
+  },
+  {
+    id: 2,
+    title: 'Gotham Nightshade Soup',
+    image:
+      'Description: A dark, rich soup with edible nightshade berries, with a swirl of cream forming the Bat-Signal.',
+    time: '45 minutes',
+    description:
+      'Dive into the mysterious flavors of Gotham with this Nightshade Soup. The cream swirls into the Bat-Signal, offering a nod to the Dark Knight.',
+    isVegan: false,
+  },
+  {
+    id: 3,
+    title: "Joker's Wild Berry Parfait",
+    image:
+      "Description: Layers of green and purple yogurt, topped with wild berries and a sprinkle of nuts, capturing the essence of the Joker's chaotic style.",
+    time: '15 minutes',
+    description:
+      'This vibrant parfait is a playful homage to the Joker, with colorful layers and a wild mix of flavors.',
+    isVegan: true,
+  },
+  {
+    id: 4,
+    title: 'Batmobile Smoothie Bowl',
+    image:
+      'Description: A sleek, black charcoal smoothie bowl, garnished with banana wheels and a trail of mixed berries.',
+    time: '20 minutes',
+    description:
+      'Fuel up like Batman with this Batmobile-inspired smoothie bowl, featuring a bold design and nutritious ingredients.',
+    isVegan: true,
+  },
+  {
+    id: 5,
+    title: "Riddler's Puzzle Pizza",
+    image:
+      'Description: A pizza with question mark-shaped pepperoni slices and a maze of cheese.',
+    time: '40 minutes',
+    description:
+      "Solve the Riddler's culinary riddle with this uniquely designed pizza, challenging and delicious in equal measure.",
+    isVegan: false,
+  },
+];
